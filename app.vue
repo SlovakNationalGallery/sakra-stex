21a8950a3a8010673eac7a7fe01f012d7ada145a
Ernest Walzel <ernest.walzel@sng.sk>
2023-06-01 17:30:12 +0200

    Basic OpenseaDragon implementation

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup>
+const { data: tileSources } = await useFetch(
+  "http://imi.sng.cust.eea.sk/publicIS/fcgi-bin/iipsrv.fcgi?IIIF=/SNGFOTOFRESHFILM/X100/SNG--P_53--1_9--2015_07_30--L2_WEB.jp2/info.json"
+);
+</script>
+
 <template>
-  <div>
-    <NuxtLink to="/" class="text-blue-400">Sakra Stex</NuxtLink>
-  </div>
+  <ClientOnly>
+    <ZoomViewer :tile-sources="tileSources" />
+  </ClientOnly>
 </template>
