a7b4c921b6ef2015a89ea01376b4c80fd3f82e7a
Ernest Walzel <ernest.walzel@sng.sk>
2023-06-01 16:10:52 +0200

    Clean up after masonry-wall experiment

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
     <NuxtLink to="/" class="text-blue-400">Sakra Stex</NuxtLink>
-    <NuxtPage />
   </div>
 </template>
--- utils/apiUtils.js
+++
@@ -1,9 +0,0 @@
-export function buildArtwork(apiResponse) {
-  return {
-    ...apiResponse,
-    content: {
-      ...apiResponse.content,
-      thumbnail: `https://www.webumenia.sk/dielo/nahlad/${apiResponse.id}/600`,
-    },
-  };
-}
