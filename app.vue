26ee5674e8a0f869b93cd4d617f57f45d0fe0820
Ernest Walzel <ernest.walzel@sng.sk>
2023-06-08 17:32:25 +0200

    Custom tour controls

--- app.vue
+++ app.vue
@@ -1,5 +1,36 @@
 <template>
   <ClientOnly>
-    <Micrio class="w-100 h-screen" id="NwEkJ" />
+    <div class="relative w-100 h-screen">
+      <Micrio id="NwEkJ" v-slot="micrio">
+        <div class="absolute inset-x-0 bottom-0 flex justify-center p-12">
+          <Transition
+            enter-from-class="opacity-0 translate-y-24"
+            enter-to-class="opacity-100"
+            enter-active-class="transition-all duration-500"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0 translate-y-24"
+            leave-active-class="transition-all duration-500"
+          >
+            <div
+              v-if="micrio.tour"
+              class="w-60 h-24 bg-green-500 grid grid-cols-3 p-2 gap-2"
+            >
+              <button
+                class="bg-black text-white"
+                @click="micrio.previousMarker"
+              >
+                &lt;
+              </button>
+              <button class="bg-black text-white" @click="micrio.cancelTour">
+                x
+              </button>
+              <button class="bg-black text-white" @click="micrio.nextMarker">
+                &gt;
+              </button>
+            </div>
+          </Transition>
+        </div>
+      </Micrio>
+    </div>
   </ClientOnly>
 </template>
--- components/Micrio.vue
+++ components/Micrio.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { HTMLMicrioElement } from "Micrio";
+import type { HTMLMicrioElement, Models } from "Micrio";
 
 const { id } = defineProps(["id"]);
 useHead({
@@ -8,21 +8,55 @@ useHead({
 });
 
 const micrio = ref<HTMLMicrioElement>();
+const tour = ref<
+  Models.ImageCultureData.MarkerTour | Models.ImageCultureData.VideoTour
+>();
 
 onMounted(() => {
-  document.querySelector("micr-io")!.addEventListener("show", () => {
+  const element = document.querySelector("micr-io")!;
+  element.addEventListener("show", () => {
     micrio.value = document.querySelector("micr-io") as HTMLMicrioElement;
   });
-  document
-    .querySelector("micr-io")!
-    .addEventListener("marker-open", (e: any) => {
-      // Clicking on a marker also opens its tour
-      const markerId = e.detail.id as string;
-      const tour = micrio.value!.$current.$data.markerTours[0];
-      const tourStep = tour.steps.findIndex((step) => step === markerId);
-      micrio.value!.state.tour.set({ ...tour, currentStep: tourStep });
-    });
+
+  element.addEventListener("marker-open", (e: any) => {
+    // Clicking on a marker also opens its tour
+    const markerId = e.detail.id as string;
+    const tour = micrio.value!.$current.$data.markerTours[0];
+    const tourStep = tour.steps.findIndex((step) => step === markerId);
+    micrio.value?.state.tour.set({ ...tour, currentStep: tourStep });
+  });
+
+  element.addEventListener("update", (e: any) => {
+    tour.value = micrio.value?.state.$tour;
+  });
 });
+
+function cancelTour() {
+  micrio.value?.state.tour.set(undefined);
+}
+
+function changeStepBy(delta: number) {
+  const tour = micrio.value?.state.$tour as
+    | Models.ImageCultureData.MarkerTour
+    | undefined;
+
+  if (tour?.currentStep === undefined) return;
+
+  const stepIndex = (() => {
+    const maxIndex = tour.steps.length - 1;
+    const nextIndex = tour.currentStep + delta;
+
+    if (nextIndex < 0) return maxIndex - 1 - (nextIndex % maxIndex);
+    if (nextIndex > maxIndex) return (nextIndex % maxIndex) - 1;
+    return nextIndex;
+  })();
+  const stepId = tour.steps[stepIndex];
+  const marker = micrio.value?.$current.$data.markers.find(
+    (marker) => marker.id === stepId
+  );
+
+  micrio.value?.state.marker.set(marker);
+}
 </script>
 
 <template>
@@ -35,4 +69,10 @@ onMounted(() => {
     toolbar="false"
     minimap="false"
   />
+  <slot
+    :tour="tour"
+    :cancelTour="cancelTour"
+    :nextMarker="() => changeStepBy(1)"
+    :previousMarker="() => changeStepBy(-1)"
+  ></slot>
 </template>
