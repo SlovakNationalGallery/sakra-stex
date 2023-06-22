2f53421f0ff9651b037725fd4dc9621b8cde2f47
Ernest Walzel <ernest.walzel@sng.sk>
2023-06-22 13:26:37 +0200

    Basic error handling

--- app.vue
+++ app.vue
@@ -32,80 +32,86 @@ function onMarkerOpen() {
   showIntro.value = false;
   showLangSwitch.value = false;
 }
+
+function onMicrioError() {
+  window.location.reload();
+}
 </script>
 
 <template>
   <div class="bg-black absolute inset-0 overflow-hidden font-body">
     <div class="h-full w-full">
       <ClientOnly>
-        <Micrio
-          id="aYdqm"
-          :cancel-tour-after-ms="20000"
-          :lang="lang"
-          :coordinates="showIntro ? [0.06, 0.5] : undefined"
-          v-slot="micrio"
-          @marker-open="onMarkerOpen"
-          @tour-started="tourRunning = true"
-          @tour-stop="tourRunning = false"
-        >
-          <!-- Controls -->
-          <div
-            class="absolute inset-x-0 bottom-0 flex justify-end p-16 pointer-events-none overflow-hidden"
+        <NuxtErrorBoundary @error="onMicrioError">
+          <Micrio
+            id="aYdqm"
+            :cancel-tour-after-ms="20000"
+            :lang="lang"
+            :coordinates="showIntro ? [0.06, 0.5] : undefined"
+            v-slot="micrio"
+            @marker-open="onMarkerOpen"
+            @tour-started="tourRunning = true"
+            @tour-stop="tourRunning = false"
           >
-            <Transition
-              enter-from-class="opacity-0"
-              enter-to-class="opacity-100"
-              enter-active-class="transition-all duration-200 ease-out"
-              leave-from-class="opacity-100"
-              leave-to-class="opacity-0"
-              leave-active-class="transition-all duration-100 ease-in"
+            <!-- Controls -->
+            <div
+              class="absolute inset-x-0 bottom-0 flex justify-end p-16 pointer-events-none overflow-hidden"
             >
-              <div
-                v-if="tourRunning"
-                class="p-4 bg-white grid grid-cols-3 rounded-[5rem] gap-4 pointer-events-auto drop-shadow-lg shadow-black/70"
+              <Transition
+                enter-from-class="opacity-0"
+                enter-to-class="opacity-100"
+                enter-active-class="transition-all duration-200 ease-out"
+                leave-from-class="opacity-100"
+                leave-to-class="opacity-0"
+                leave-active-class="transition-all duration-100 ease-in"
               >
-                <button
-                  class="w-14 h-14 bg-black/5 active:bg-black/10 flex items-center justify-center rounded-full"
-                  @click="micrio.cancelTour"
+                <div
+                  v-if="tourRunning"
+                  class="p-4 bg-white grid grid-cols-3 rounded-[5rem] gap-4 pointer-events-auto drop-shadow-lg shadow-black/70"
                 >
-                  <img src="~/assets/img/x-mark.svg" />
-                </button>
+                  <button
+                    class="w-14 h-14 bg-black/5 active:bg-black/10 flex items-center justify-center rounded-full"
+                    @click="micrio.cancelTour"
+                  >
+                    <img src="~/assets/img/x-mark.svg" />
+                  </button>
+                  <button
+                    class="w-14 h-14 bg-black/5 active:bg-black/10 flex items-center justify-center rounded-full"
+                    @click="micrio.previousMarker"
+                  >
+                    <img src="~/assets/img/arrow-left.svg" />
+                  </button>
+                  <button
+                    class="w-14 h-14 bg-black/5 active:bg-black/10 flex items-center justify-center rounded-full"
+                    @click="micrio.nextMarker"
+                  >
+                    <img src="~/assets/img/arrow-left.svg" class="rotate-180" />
+                  </button>
+                </div>
+              </Transition>
+            </div>
+
+            <!-- Lang switcher -->
+            <Transition
+              appear
+              enter-from-class="opacity-0 translate-y-12"
+              enter-to-class="opacity-100"
+              enter-active-class="transition-all duration-300 delay-200 ease-out"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0 translate-y-12"
+              leave-active-class="transition-all duration-300"
+            >
+              <div v-if="showLangSwitch" class="hidden absolute top-0 right-0">
                 <button
-                  class="w-14 h-14 bg-black/5 active:bg-black/10 flex items-center justify-center rounded-full"
-                  @click="micrio.previousMarker"
+                  class="text-white font-display text-3xl uppercase p-12"
+                  @click="lang = lang === 'sk' ? 'en' : 'sk'"
                 >
-                  <img src="~/assets/img/arrow-left.svg" />
-                </button>
-                <button
-                  class="w-14 h-14 bg-black/5 active:bg-black/10 flex items-center justify-center rounded-full"
-                  @click="micrio.nextMarker"
-                >
-                  <img src="~/assets/img/arrow-left.svg" class="rotate-180" />
+                  {{ lang }}
                 </button>
               </div>
             </Transition>
-          </div>
-
-          <!-- Lang switcher -->
-          <Transition
-            appear
-            enter-from-class="opacity-0 translate-y-12"
-            enter-to-class="opacity-100"
-            enter-active-class="transition-all duration-300 delay-200 ease-out"
-            leave-from-class="opacity-100"
-            leave-to-class="opacity-0 translate-y-12"
-            leave-active-class="transition-all duration-300"
-          >
-            <div v-if="showLangSwitch" class="hidden absolute top-0 right-0">
-              <button
-                class="text-white font-display text-3xl uppercase p-12"
-                @click="lang = lang === 'sk' ? 'en' : 'sk'"
-              >
-                {{ lang }}
-              </button>
-            </div>
-          </Transition>
-        </Micrio>
+          </Micrio>
+        </NuxtErrorBoundary>
       </ClientOnly>
     </div>
 
